d6e689f30cb0719d4c908020078a84b8baf6c4ed
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-07 16:32:18 -0500

    Working on Inheritance

--- WorkShop02Console/Program.cs
+++ WorkShop02Console/Program.cs
@@ -9,9 +9,10 @@ try
     var rhombus = new Rhombus(nameof(Rhombus), 5, 7, 10);
     var kite = new Kite(nameof(Kite), 7, 6, 5, 8);
     var rectangle = new Rectangle(nameof(Rectangle), 4.568, 67.790);
+    var triangle = new Triangle(nameof(Triangle), 45.56, 12.34, 27.09, 15);
 
 
-    var figures = new List<GeometricFigure>() { circle, square, rhombus, kite, rectangle };
+    var figures = new List<GeometricFigure>() { circle, square, rhombus, kite, rectangle, triangle };
     foreach (var figure in figures)
     {
         Console.WriteLine(figure);
